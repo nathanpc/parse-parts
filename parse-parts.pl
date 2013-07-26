a1c486a022f17a250ed31701728e60ba6b1753ec
Nathan Campos <eeepc904@gmail.com>
2013-07-26 19:32:56 -0300

    I LIEK PERL

---
+++ parse-parts.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl -w
+
+#######################################################################
+# File:   parse-parts.pl
+# Author: Nathan Campos <nathanpc@dreamintech.net>
+#
+# A program to help you organize and keep track of all the electronics
+# parts you've ordered.
+#######################################################################
+
