f2b84b5a575e8a8897f0e498f25bf29e33fbbae1
Nathan Campos <eeepc904@gmail.com>
2013-10-09 07:57:33 -0300

    Finished implementing everything related to DigiKey

--- DatabaseHelper/DigiKey.pm
+++ DatabaseHelper/DigiKey.pm
@@ -54,19 +54,29 @@ sub add_part {
 	my ($self, $order_ref, $quantity, $part_ref, $part_name, $description) = @_;
 
 	# Check if the part already exist in the database.
-	my $sth = $self->{parts_db}->prepare("SELECT store, orders, part_number FROM Parts WHERE part_number LIKE '%$part_ref%'");
-	$sth->execute();
+	my $sel_sth = $self->{parts_db}->prepare("SELECT orders, part_number, quantity FROM Parts WHERE part_number LIKE '%$part_ref%'");
+	$sel_sth->execute();
 
-	if (defined $sth->fetchrow_arrayref()) {
+	my $entry_ref = $sel_sth->fetchrow_arrayref();
+	if (defined $entry_ref) {
 		# Update a part in the database.
-		print colored("Updated: ", "blue"), "$description\n";
+		my $db_order_ref = @{ $entry_ref }[0];
 
-		# TODO: Remember that the "store", "orders" and "part_number" fields are arrays.
-		# TODO: Change the "store" field to a text type.
+		if ($db_order_ref !~ /$order_ref/i) {
+			# The order ref is completely new.
+			$db_order_ref .= ", $order_ref";
+
+			$self->{parts_db}->do("UPDATE Parts SET orders = '$db_order_ref' WHERE part_number LIKE '%$part_ref%'");
+			print colored("Updated: ", "blue"), "$description\n";
+		} else {
+			print colored("No change: ", "cyan"), "$description\n";
+		}
 	} else {
 		# Insert a new part in th database.
+		my $sth = $self->{parts_db}->prepare("INSERT INTO Parts VALUES(NULL, 'DigiKey', ?, ?, ?, ?, ?)");
+		$sth->execute($order_ref, $quantity, $part_ref, $part_name, $description);
+
 		print colored("Added: ", "green"), "$description\n";
-		$self->{parts_db}->do("INSERT INTO Parts VALUES(NULL, 'DigiKey', '$order_ref', $quantity, '$part_ref', '$part_name', '$description')");
 	}
 }
 
